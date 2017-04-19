992e65cc821e190a716afe3230c9921891cbd6ed
aasish-r <aasishrampalli1997@gmail.com>
2017-04-19 22:51:35 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    // Important declaration
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+
+    /*  Create the network socket.   */
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+
+    /*  1) PR_INET Internet domain
+        2) SOCK_STREAM represents Stream socket
+        3) 0 represents Default protocol (TCP in this case)
+        */
+
+    // Configure settings of the server address
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+
+    /* Set port number */
+    serverAddr.sin_port = htons(9000);
+    /*  In networking we prefer to use big-endian binary
+        sequencing.  Since in our PC we use small endian binary
+        scheme, we use htons to convert little endian to big
+        endian.
+        */
+
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr("172.17.46.123");
+
+    /* Set all bits of the padding field to 0 */
+    memset(serverAddr.sin_zero, '\0', sizeof
+            serverAddr.sin_zero);
+
+    /* Connect the socket to the server using the address*/
+    addr_size = sizeof serverAddr;
+    if ( connect( clientSocket, (struct sockaddr *) &serverAddr,
+                addr_size) < 0 )
+    {
+        printf("[ERROR] Unable to connect to server.\n");
+        close( clientSocket );
+        exit(0);
+    }
+    close( clientSocket );
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Important declaration
+    int serverSocket = 0, newSocket = 0;
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddress;
+    socklen_t addr_size;
+
+    /*  Create the network socket.   */
+    serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+    // Configure settings of the server address
+    /* Address family = Internet */
+    serverAddr.sin_family = AF_INET;
+    /* Set port number */
+    serverAddr.sin_port = htons(9000);
+    /* Set IP address to localhost */
+    serverAddr.sin_addr.s_addr = inet_addr ( "172.17.46.123" );
+    /* Set all bits of the padding field to 0 */
+    memset ( serverAddr.sin_zero, '\0', sizeof (serverAddr.sin_zero) );
+
+    /*---- Bind the address struct to the socket ----*/
+    if ( bind ( serverSocket, ( struct sockaddr *) &serverAddr, sizeof ( serverAddr ) )  < 0)
+        printf("[ERROR] Socket binding Failed. \n");
+
+    /* Listen on the socket, with 5 max connection requests queued */
+    if ( listen ( serverSocket, 5 ) ==0 )
+        printf ( "Server Scocket Initiated. Listening to its clients : \n" );
+    else
+        printf("[ERROR] More than limit.\n");
+
+    /* Accept call creates a new socket for the incoming connection */
+    newSocket = accept ( serverSocket, (struct sockaddr *) &clientAddress, &addr_size);
+    while ( 1 )
+    {
+        addr_size = sizeof ( clientAddress );
+        if ( newSocket < 0)
+            printf("[ERROR] Failed to connect to game.\n");
+
+        printf("Joining client:  %s\n", inet_ntoa(clientAddress.sin_addr), randomNumber );
+        sleep ( 1 );
+        break;
+    }
+    close ( newSocket );
+    close ( serverSocket );
+    return 0;
+}
